ab99fe938571f4bf83766e373ea93396f13c3181
ace <falls.16177@gmail.com>
2025-10-05 14:50:30 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
+import  java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String Password;
+
+        while (true){
+            System.out.print("Enter your Password: ");
+            Password = input.nextLine();
+            if (Password.length() >=6){
+                System.out.println("Your Password is accepted");
+                break;
+            } else System.out.println("Your Password is too short try again");
+        }
+    }
 }
